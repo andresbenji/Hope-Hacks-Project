b1691309daed8fb99322e745d072bada6993725e
andresbenji <116680825+andresbenji@users.noreply.github.com>
2023-06-22 09:56:28 -0400

    testing

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
 const port = 6969;
-const fs = require("fs");
+const axios = require("axios");
+const url =
+  "https://numbersapi.p.rapidapi.com/4/trivia?fragment=true&notfound=floor&json=true";
+
+const getTrivia = app.get("/api/data", async (req, res) => {
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch data from the API." });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
