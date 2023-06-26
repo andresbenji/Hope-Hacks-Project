c9a7f1d2f72ab2f75fdee11c52f125a7d5e11f82
oscmor8 <oscarmoradoo@gmail.com>
2023-06-26 11:35:50 -0400

    font end pushes

--- routes/routes.js
+++ routes/routes.js
@@ -1,30 +1,27 @@
-const express = require("express");
-const path = require("path");
+// const express = require("express");
+// const path = require("path");
 
-const app = express();
-let publicPath = path.join(__dirname, "public");
+// const app = express();
+// let publicPath = path.join(__dirname, "public");
 
-app.get("/", (req, res) => {
-  res.sendFile(`${publicPath}/index.html`);
-});
-app.get("/", (req, res) => {
-  res.sendFile(`${publicPath}/index.html`);
-});
-app.get("/", (req, res) => {
-  res.sendFile(`${publicPath}/index.html`);
-});
-app.get("/", (req, res) => {
-  res.sendFile(`${publicPath}/index.html`);
-});
-app.get("/", (req, res) => {
-  res.sendFile(`${publicPath}/index.html`);
-});
-app.get("/", (req, res) => {
-  res.sendFile(`${publicPath}/index.html`);
-});
-app.get("/", (req, res) => {
-  res.sendFile(`${publicPath}/index.html`);
-});
-app.get("/", (req, res) => {
-  res.sendFile(`${publicPath}/index.html`);
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(`${publicPath}/index.html`);
+// });
+// app.get("/randomfact", (req, res) => {
+//   res.sendFile(`${publicPath}/randomfact.html`);
+// });
+// app.get("/mathFact", (req, res) => {
+//   res.sendFile(`${publicPath}/mathFact.html`);
+// });
+// app.get("/yearfact", (req, res) => {
+//   res.sendFile(`${publicPath}/year.html`);
+// });
+// app.get("/triviafact", (req, res) => {
+//   res.sendFile(`${publicPath}/triviafact.html`);
+// });
+// app.get("/team", (req, res) => {
+//   res.sendFile(`${publicPath}/team.html`);
+// });
+// app.get("/contact", (req, res) => {
+//   res.sendFile(`${publicPath}/.html`);
+// });
